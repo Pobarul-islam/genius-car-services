d83435231b02fa4e1da429eb95fe333ba9c57868
Pobarul-islam <pobarul3@gmail.com>
2022-04-12 18:53:27 +0600

    start

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const services = [
+    {id: 1, name: 'oil Change', Price: 100, description: '',img: " " }
+]
+const Services = () => {
+    return (
+        <div>
+            <h2>This is Services Page</h2>
+        </div>
+    );
+};
+
+export default Services;
